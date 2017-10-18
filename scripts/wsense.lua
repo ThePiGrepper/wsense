5b0ef49388b68a7353acdda33a595cdfb1c46d6c
Erick Cafferata <lcafferatac@gmail.com>
2017-10-18 10:41:12 -0500

    modularizes wsense functions

--- scripts/wsense.lua
+++ scripts/wsense.lua
@@ -18,6 +18,19 @@ function trim(s)
   return (string.gsub(s, "^%s*(.-)%s*$", "%1"))
 end
 
+function ls(dir)
+	if dir == "flash" then
+		file.chdir('/SD0')
+	end
+	l = file.list();
+	for k,v in pairs(l) do
+		print("name:"..k..", size:"..v)
+	end
+	file.chdir('/FLASH')
+end
+
+-- html server functions
+
 -- hx711 functions
 --
 function hx711_resume(period) --miliseconds
@@ -90,64 +103,69 @@ function get_time()
   return mon..date..hour..min
 end
 
--- Start
---
--- I2C/RTC setup
-i2c.setup(id, sda, scl, i2c.SLOW)
--- SD control functions
---
-spi.setup(1, spi.MASTER, spi.CPOL_LOW, spi.CPHA_LOW, 8, 8)
+function wsense_setup()
+	local go_flag = true
+	-- I2C/RTC setup
+	i2c.setup(id, sda, scl, i2c.SLOW)
+	-- SD control functions
+	spi.setup(1, spi.MASTER, spi.CPOL_LOW, spi.CPHA_LOW, 8, 8)
+	-- then mount the sd
+	-- note: the card initialization process during `file.mount()` will set spi divider temporarily to 200 (400 kHz)
+	-- it's reverted back to the current user setting before `file.mount()` finishes
+	vol = file.mount("/SD0", 8)   -- 2nd parameter is optional for non-standard SS/CS pin
+	if not vol then
+		print("retry mounting")
+		vol = file.mount("/SD0", 8)
+		if not vol then
+			-- TODO: add a retry counter and a restart().(failed X times, do idle 4ver.
+			--error("mount failed")
+			go_flag = false
+		end
+	end
+	return go_flag
+end
 
--- initialize other spi slaves
-
--- then mount the sd
--- note: the card initialization process during `file.mount()` will set spi divider temporarily to 200 (400 kHz)
--- it's reverted back to the current user setting before `file.mount()` finishes
-vol = file.mount("/SD0", 8)   -- 2nd parameter is optional for non-standard SS/CS pin
-if not vol then
-  print("retry mounting")
-  vol = file.mount("/SD0", 8)
-  if not vol then
-    -- TODO: add a retry counter and a restart().(failed X times, do idle 4ver.
-    error("mount failed")
+function wsense_init()
+	local go_flag = true
+	if wsense_setup() == false then
+    error("HW setup failed")
     go_flag = false
   end
-end
+	-- init program
+	-- load variables
+	-- check config files
+	-- PREFIX config file
+	f = file.open("PREFIX")
+	if not f then
+		print("PREFIX file not found")
+		prefix = ""
+		go_flag = false
+	else
+		prefix = trim(file.read())
+		file.close()
+	end
+	--NEXTINDEX config file
+	f = file.open("NEXTINDEX")
+	if not f then
+		print("Creating NEW NEXTINDEX file")
+		f = file.open("NEXTINDEX","w")
+		file.writeline('1')
+		file.close()
+		index = "0"
+	else
+		index = trim(file.read())
+		file.close()
+		file.open("NEXTINDEX","w")
+		local tmp = index + 1
+		file.writeline(string.format("%d",tmp))
+		file.close()
+	end
 
--- init program
--- load variables
--- check config files
--- PREFIX config file
-f = file.open("PREFIX") 
-if not f then
-  print("PREFIX file not found")
-	prefix = ""
-  go_flag = false
-else
-	prefix = trim(file.read())
-	file.close()
-end
---NEXTINDEX config file
-f = file.open("NEXTINDEX")
-if not f then
-	print("Creating NEW NEXTINDEX file")
-	f = file.open("NEXTINDEX","w")
-	file.writeline('1')
-	file.close()
-	index = "0"
-else
-	index = trim(file.read())
-	file.close()
-	file.open("NEXTINDEX","w")
-	local tmp = index + 1
-	file.writeline(string.format("%d",tmp))
-	file.close()
-end
+	-- THIS IS A BUG, W/O THIS PRINT IT BREAKS
+	print('dbg:SD mounted successfully...')
+	rtcnow = get_time()
+	filename = prefix..string.format("%03d",index)..rtcnow
+	print("new file "..filename.." opened")
 
--- THIS IS A BUG, W/O THIS PRINT IT BREAKS
-print('dbg:SD mounted successfully...')
-rtcnow = get_time()
-filename = prefix..string.format("%03d",index)..rtcnow
-print("new file "..filename.." opened")
-
-hx711_start(period)
+	hx711_start(period)
+end
